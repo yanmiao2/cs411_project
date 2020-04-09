b8f2d8a51d1adcd5b4dbd6c325b07dfea42fe848
Yan Miao <yanmiao@10-151-218-210.vlan912.client.wireless.uic.edu>
2020-04-09 17:00:41 -0500

    basic CRUD on admin & search on user

--- models/Event.js
+++ models/Event.js
@@ -3,10 +3,14 @@ var Schema = mongoose.Schema;
 
 var myschema = new Schema({
     event_name: String,
-    event_date: String,
-    price: String,
-    duration: String,
-    type: String,
+    event_date: String, // yyyy/mm/dd
+    ticket_price: String,
+    event_size: String,
+    start_time: String,  // 24hr
+    location: String,
+    web_link: String,
+    description: String,
+    event_type: String // e.g. "romance sports"
 });
 
 module.exports = mongoose.model('Event', myschema,"events");
--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,151 @@ router.get('/', (req, res) => {
     );
 });
 
+router.get('/events', (req, res) => {
+    Event.find(
+        {},
+        (err, data) => {
+            if(err) console.log(err);
+            else{
+                res.render('userEvents',{ events : data })
+            }
+        }
+    );
+});
+
+router.post('/events', (req, res) => {
+    Event.find(
+        {},
+        (err, data) => {
+            if(err) console.log(err);
+            else{
+                res.render('userEvents',{ events : data })
+            }
+        }
+    );
+});
+
+router.post('/events/details', (req, res) => {
+    Event.find(
+        {},
+        (err, data) => {
+            if(err) console.log(err);
+            else{
+                res.render('userEvent',{ event : data })
+            }
+        }
+    );
+});
+
+router.get('/admin', (req, res) => {
+    res.render('admin');
+});
+
+router.post('/admin', (req, res) => {
+    Event.find(
+        {},
+        (err, data) => {
+            if(err) console.log(err);
+            else{
+                if(req.body.pwd == "sights")
+                    res.render('adminEvents',{ events : data })
+                else
+                    res.redirect('/admin')
+            }
+        }
+    );
+});
+
+router.post('/admin/events', (req, res) => {
+    Event.find(
+        {_id: req.body.eventID},
+        (err, data) => {
+            if(err) console.log(err);
+            else{
+                res.render('adminEvent',{ event : data })
+            }
+        }
+    );
+});
+
+router.get('/admin/add', (req, res) => {
+    res.render('adminAddEvent');
+});
+
+router.post('/admin/add', (req, res) => {
+    const {event_name,event_date,ticket_price,event_size,start_time,location,web_link,event_type}= req.body;
+    const newEvent = {event_name,event_date,ticket_price,event_size,start_time,location,web_link,event_type}
+    var data = Event(newEvent);
+    if(event_name,event_date,location){
+        data.save(function(err) {
+            if (err) {
+            console.log("Error in Insert Record");
+            } else {
+                Event.find(
+                    {},
+                    (err, data) => {
+                        if(err) console.log(err);
+                        else{
+                            res.render('adminEvents',{ events : data })
+                        }
+                    }
+                );
+            }
+        });
+    }else{
+        console.log("please enter all fields");
+        res.render('adminAddEvent');
+    }
+});
+
+router.post('/admin/update', (req, res) => {
+    const {event_name,event_date,ticket_price,event_size,start_time,location,web_link,description,event_type}= req.body;
+    Event.findOneAndUpdate(
+        {_id:req.body.eventID},
+        {event_name: event_name,
+            event_date: event_date,
+            ticket_price: ticket_price,
+            event_size: event_size,
+            start_time: start_time,
+            location: location,
+            web_link: web_link,
+            description: description,
+            event_type: event_type},
+        function(err, my_res) {
+            if (err) {
+                console.log("Error in Fetch Data " + err);
+            } else {
+                Event.find(
+                    {},
+                    (err, data) => {
+                        if(err) console.log(err);
+                        else{
+                            res.render('adminEvents',{ events : data })
+                        }
+                    }
+                );
+            }
+        });
+});
+router.post("/admin/delete",(req, res) => {
+    Event.findOneAndDelete(
+        {_id:req.body.eventID},
+        function(err, my_res) {
+            if (err) {
+                console.log("Error in Fetch Data " + err);
+            } else {
+                Event.find(
+                    {},
+                    (err, data) => {
+                        if(err) console.log(err);
+                        else{
+                            res.render('adminEvents',{ events : data })
+                        }
+                    }
+                );
+            }
+        });
+});
+
+// add update delete
 module.exports = router;
